83d54fe81e1258d2a3337acf5a5187d93df65bc0
Liu, Xu <xliu701@gatech.edu>
2019-11-05 16:45:05 -0500

    add files to initialize the backend

---
+++ backend/src/test/java/cs6400/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cs6400.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
